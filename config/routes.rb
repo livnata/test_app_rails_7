6d532fa92839d21989620236c4d26661853a266a
livnata <livnat.avikasis@gmail.com>
2022-11-02 16:49:03 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root 'pages#home' # pages controller in the controllers folder and the action will be called home inside it
+end
